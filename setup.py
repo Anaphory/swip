74c7201b62c3a8b222f12712fb6ce65b07242407
Gereon Kaiping <anaphory@yahoo.de>
2017-09-03 01:14:52 +0200

    Add package infrastructure

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='swip',
+    version="0.1.1",
+    description='SignWriting Images in Python',
+    long_description=open("README.md").read().split('##')[0],
+    author='Gereon Kaiping',
+    author_email='g.a.kaiping@hum.leidenuniv.nl',
+    url='https://github.com/Anaphory/swip',
+    install_requires=[
+        # 'sqlite3',
+    ],
+    include_package_data=True,
+    license="MIT",
+    zip_safe=False,
+    keywords='',
+    classifiers=[
+        ],
+    packages=[
+        'swip'],
+    entry_points={
+        'console_scripts': [
+            'swip=swip',
+        ]
+    },
+    tests_require=['nose'],
+)
